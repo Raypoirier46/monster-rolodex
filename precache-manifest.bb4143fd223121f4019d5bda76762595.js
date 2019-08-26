252f69e64e8c6f3328b9eccb20d52e05adc6f9a3
Raymond Poirier <rock_on_46@hotmail.com>
2019-08-25 19:35:43 -0600

    Updates

---
+++ precache-manifest.bb4143fd223121f4019d5bda76762595.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbc43d0377d6adc0347ff5036cb682ea",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bc554690c5bd26f1ac10",
+    "url": "/monster-rolodex/static/css/main.10befba1.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/monster-rolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "bc554690c5bd26f1ac10",
+    "url": "/monster-rolodex/static/js/main.b1a1c53e.chunk.js"
+  },
+  {
+    "revision": "384d151a98451564c735",
+    "url": "/monster-rolodex/static/js/runtime~main.50883f11.js"
+  }
+]);
